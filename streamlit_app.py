fc060fdf5cc7710dadddced3720abe5d3d6f0fc3
Vyshnavi Vuyyuru <103961945+vyshnavi-03@users.noreply.github.com>
2022-04-18 14:50:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text('Hard-Boiled Free-Range Egg')
