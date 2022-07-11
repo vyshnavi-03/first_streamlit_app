67a56c01aff35d35cd74cbea094a87924d5135cd
Vyshnavi Vuyyuru <103961945+vyshnavi-03@users.noreply.github.com>
2022-07-11 13:42:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
 #don't run anything past here while we troubleshoot
 streamlit.stop()
 
